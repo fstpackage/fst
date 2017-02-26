a7513c21f271533c6c7aa6e8ceeaa815e718a47d
MarkKlik <markklik@gmail.com>
2017-02-26 23:48:20 +0100

    Move v0.7.2 code for reading fst files into deprecated directory

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,28 +5,6 @@
 
 using namespace Rcpp;
 
-// boolToInt
-SEXP boolToInt(SEXP logical);
-RcppExport SEXP fst_boolToInt(SEXP logicalSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type logical(logicalSEXP);
-    rcpp_result_gen = Rcpp::wrap(boolToInt(logical));
-    return rcpp_result_gen;
-END_RCPP
-}
-// intToBool
-SEXP intToBool(SEXP ints);
-RcppExport SEXP fst_intToBool(SEXP intsSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type ints(intsSEXP);
-    rcpp_result_gen = Rcpp::wrap(intToBool(ints));
-    return rcpp_result_gen;
-END_RCPP
-}
 // fstStore
 SEXP fstStore(String fileName, SEXP table, SEXP compression);
 RcppExport SEXP fst_fstStore(SEXP fileNameSEXP, SEXP tableSEXP, SEXP compressionSEXP) {
@@ -62,83 +40,6 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< SEXP >::type startRow(startRowSEXP);
     Rcpp::traits::input_parameter< SEXP >::type endRow(endRowSEXP);
     rcpp_result_gen = Rcpp::wrap(fstRead(fileName, columnSelection, startRow, endRow));
-    return rcpp_result_gen;
-END_RCPP
-}
-// SType
-int SType(SEXP value);
-RcppExport SEXP fst_SType(SEXP valueSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type value(valueSEXP);
-    rcpp_result_gen = Rcpp::wrap(SType(value));
-    return rcpp_result_gen;
-END_RCPP
-}
-// compChar
-int compChar(SEXP str1, SEXP str2);
-RcppExport SEXP fst_compChar(SEXP str1SEXP, SEXP str2SEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type str1(str1SEXP);
-    Rcpp::traits::input_parameter< SEXP >::type str2(str2SEXP);
-    rcpp_result_gen = Rcpp::wrap(compChar(str1, str2));
-    return rcpp_result_gen;
-END_RCPP
-}
-// IsSortedTable
-bool IsSortedTable(SEXP table, SEXP key);
-RcppExport SEXP fst_IsSortedTable(SEXP tableSEXP, SEXP keySEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type table(tableSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type key(keySEXP);
-    rcpp_result_gen = Rcpp::wrap(IsSortedTable(table, key));
-    return rcpp_result_gen;
-END_RCPP
-}
-// FirstIntEqualHigher
-SEXP FirstIntEqualHigher(SEXP intVec, SEXP intKey, SEXP lower, SEXP upper);
-RcppExport SEXP fst_FirstIntEqualHigher(SEXP intVecSEXP, SEXP intKeySEXP, SEXP lowerSEXP, SEXP upperSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type intVec(intVecSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type intKey(intKeySEXP);
-    Rcpp::traits::input_parameter< SEXP >::type lower(lowerSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type upper(upperSEXP);
-    rcpp_result_gen = Rcpp::wrap(FirstIntEqualHigher(intVec, intKey, lower, upper));
-    return rcpp_result_gen;
-END_RCPP
-}
-// LastIntEqualLower
-SEXP LastIntEqualLower(SEXP intVec, SEXP intKey, SEXP lower, SEXP upper);
-RcppExport SEXP fst_LastIntEqualLower(SEXP intVecSEXP, SEXP intKeySEXP, SEXP lowerSEXP, SEXP upperSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type intVec(intVecSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type intKey(intKeySEXP);
-    Rcpp::traits::input_parameter< SEXP >::type lower(lowerSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type upper(upperSEXP);
-    rcpp_result_gen = Rcpp::wrap(LastIntEqualLower(intVec, intKey, lower, upper));
-    return rcpp_result_gen;
-END_RCPP
-}
-// LowerBoundIndex
-SEXP LowerBoundIndex(SEXP table, SEXP key, SEXP lower, SEXP upper);
-RcppExport SEXP fst_LowerBoundIndex(SEXP tableSEXP, SEXP keySEXP, SEXP lowerSEXP, SEXP upperSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type table(tableSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type key(keySEXP);
-    Rcpp::traits::input_parameter< SEXP >::type lower(lowerSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type upper(upperSEXP);
-    rcpp_result_gen = Rcpp::wrap(LowerBoundIndex(table, key, lower, upper));
     return rcpp_result_gen;
 END_RCPP
 }
