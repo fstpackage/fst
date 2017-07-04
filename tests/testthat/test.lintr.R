9e162ae7bc278b92f55160a930fe768da2e0acae
Mark Klik <markklik@gmail.com>
2017-07-04 11:25:37 +0200

    Add unit test with lintr checking all code except RcppExports.R
    The object_usage_linter, camel_case_linter, commas_linter and multiple_dots_linter were disabled to avoid some lintr issues

--- tests/testthat/test.lintr.R
+++ tests/testthat/test.lintr.R
@@ -6,12 +6,38 @@ require(lintr)
 # issues with lintr:
 #   * is.data.table method from data.table not recognized
 #   * RcppExports not excluded
-#
-# if (requireNamespace("lintr", quietly = TRUE)) {
-#   test_that("Package Style", {
-#     lints <- with_defaults(line_length_linter = line_length_linter(120))
-#     lints <- lints[!(names(lints) %in% c("object_usage_linter", "camel_case_linter", "commas_linter"))]
-#
-#     expect_lint_free(linters = lints)
-#   })
-# }
+
+if (requireNamespace("lintr", quietly = TRUE)) {
+  test_that("Package Style", {
+    lints <- with_defaults(line_length_linter = line_length_linter(120))
+    lints <- lints[!(names(lints) %in%
+      c("object_usage_linter", "camel_case_linter", "commas_linter", "multiple_dots_linter"))]
+
+    codeFiles <- list.files("../..", "R$", full.names = TRUE, recursive = TRUE)
+    codeFiles <- codeFiles[codeFiles != "../../R/RcppExports.R"]  # manualy remove RcppExports file
+
+    # Calculate lintr results for all code files
+    lintResults <- lintr:::flatten_lints(lapply(codeFiles, function(file) {
+        if (interactive()) {
+            message(".", appendLF = FALSE)
+        }
+        lint(file, linters = lints, parse_settings = FALSE)
+    }))
+
+    # newline
+    if (interactive()) {
+        message()
+    }
+
+    lint_output <- NULL
+
+    if (length(lintResults) > 0) {
+      lintResults <- sapply(lintResults,
+        function(lintRes) {
+          paste(lintRes$filename, " (", lintRes$line_number, "): ", lintRes$message)
+        })
+    }
+
+    expect_true(length(lintResults) == 0, paste(lintResults, sep = "\n", collapse = "\n"))
+  })
+}
