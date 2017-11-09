58beee255d237b8e5a099b3f0370c4889f7d77cc
Mark Klik <markklik@gmail.com>
2017-11-09 09:18:46 +0100

    Change license to GNU Affero General Public License version 3

--- src/fstcore_v1/character/character_v1.cpp
+++ src/fstcore_v1/character/character_v1.cpp
@@ -5,21 +5,20 @@
 
   This file is part of fstlib.
 
-  fstlib is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+  fstlib is free software: you can redistribute it and/or modify it under the
+  terms of the GNU Affero General Public License version 3 as published by the
+  Free Software Foundation.
 
-  fstlib is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  fstlib is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+  details.
 
-  You should have received a copy of the GNU General Public License
-  along with fstlib.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Affero General Public License
+  along with fstlib. If not, see <http://www.gnu.org/licenses/>.
 
-  You can contact the author at :
-  - fstlib source repository : https://github.com/fstPackage/fstlib
+  You can contact the author at:
+  - fstlib source repository : https://github.com/fstpackage/fstlib
 */
 
 
