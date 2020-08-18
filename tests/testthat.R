111523c9e75e2ec1d86e148c7ea15d1797f920fd
markklik <markklik@gmail.com>
2020-08-18 14:20:50 +0200

    Unit tests are robust against a missing nanotime, lintr or bit64 package

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,10 +2,6 @@
 # required packages
 library(testthat)
 library(fst)
-library(data.table)
-library(lintr)
-library(bit64)
-library(nanotime)
 
 # some helper functions
 source("testthat/helper_fstwrite.R")  # cross-version testing
