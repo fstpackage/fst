e29c18dc0d7978ad900e5351c0e242f1459d7b09
MarkKlik <markklik@gmail.com>
2023-12-05 16:01:51 +0100

    Add stability tests for previous fst file versions

--- tests/testthat/test_legacy.R
+++ tests/testthat/test_legacy.R
@@ -13,3 +13,16 @@ test_that("Read legacy format", {
     "File header information does not contain the fst format marker"
   )
 })
+
+
+test_that("0.9.8-0.9.18.fst", {
+
+  # BigEndian systems will fail this test
+  skip_on_cran()
+  skip_on_ci()
+
+  expect_equal(
+    readRDS("datasets/test-set.rds"),
+    read_fst("datasets/0.9.8-0.9.18.fst")
+  )
+})
